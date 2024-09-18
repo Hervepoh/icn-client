34ee39c814a681a210b28d515e6d69b319e6fd5e
Ngando Epoh Herve <epohherve63@gmail.com>
2024-09-18 17:15:25 +0100

    Add .env.sample

---
+++ secret.ts
@@ -0,0 +1,16 @@
+
+export const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL
+
+export const NEXT_PUBLIC_SERVER_URI = process.env.NEXT_PUBLIC_SERVER_URI
+
+export const NEXT_PUBLIC_SOCKET_SERVER_URI = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
+
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
+
+export const SECRET = process.env.SECRET 
