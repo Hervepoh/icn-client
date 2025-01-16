5a8e777ab7259027951b93604bd0d1e823d0a75d
Hervepoh <epohherve63@gmail.com>
2025-01-16 14:09:19 +0000

    Add user-sheet for user management(create)

--- features/users/components/new-user-sheet.tsx
+++ features/users/components/new-user-sheet.tsx
@@ -46,7 +46,7 @@ export function NewUserSheet() {
                         name: '',
                         email: '',
                         ldap: true,
-                        roleId: ["fe91303b-9577-4638-aa2a-fd1f8c22b28e"],  // fe91303b-9577-4638-aa2a-fd1f8c22b28e is the id of role: USER FOR DEFAULT SELECT USER
+                        roleId: ["f0b4f5d1-5cee-42e5-8731-e6722635af24"],  // fe91303b-9577-4638-aa2a-fd1f8c22b28e is the id of role: USER FOR DEFAULT SELECT USER
                         password: '',
                         passwordConfirm: '',
                         unitId:''
